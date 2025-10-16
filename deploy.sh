ebfeb5ee317e9379028985b06bd317a2b51e7374
Caden218 <60429208+Caden218@users.noreply.github.com>
2025-10-17 07:40:12 +0800

    update
    update

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 
 # abort on errors
-
+set -e
 
 # build
-npm run build
+NODE_OPTIONS=--openssl-legacy-provider npm run build
 
 # navigate into the build output directory
 cd dist
